fb206503401705354e77483c904ed0a79e2f6b85
Web_1 <kli4ev2015@yandex.ru>
2021-12-08 14:03:06 +0300

    add

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from todo .models import Todo
 
-# Register your models here.
+
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+
+admin.site.register(Todo, TodoAdmin)
--- todowoo/urls.py
+++ todowoo/urls.py
@@ -1,7 +1,7 @@
 """todowoo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,13 +16,21 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from todo import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Auth  Автор
+
+    # Auth
     path('signup/', views.signupuser, name='signupuser'),
     path('login/', views.loginuser, name='loginuser'),
     path('logout/', views.logoutuser, name='logoutuser'),
-    # Todos  Задачи
-    path('current/', views.currenttodos, name='currenttodos'),
+
+    # Todos
     path('', views.home, name='home'),
+    path('create/', views.createtodo, name='createtodo'),
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('completed/', views.completedtodos, name='completedtodos'),
+    path('todo/<int:todo_pk>', views.viewtodo, name='viewtodo'),
+    path('todo/<int:todo_pk>/complete', views.completetodo, name='completetodo'),
+    path('todo/<int:todo_pk>/delete', views.deletetodo, name='deletetodo'),
 ]
